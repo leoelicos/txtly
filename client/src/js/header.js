72018dea0907bcccfb7ede7b2c9603a7f3ae3227
Leo Wong <leoelicos@gmail.com>
2023-04-18 21:50:31 +1000

    Replace with txtly, markdown, javascript

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,12 +1,10 @@
 const header = `
 /*
-       _____  ____________
-      / /   |/_  __/ ____/
- __  / / /| | / / / __/   
-/ /_/ / ___ |/ / / /___   
- ____/_/  |_/_/ /_____/   
-just another text editor
-*/                          
-`;
+Txtly
+*/   
+# markdown supported
+const javascript = 'supported'
+                       
+`
 
-export { header };
+export { header }
